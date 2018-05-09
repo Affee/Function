2d340578c145390e77c016e257e880e96209c575
Affee <304442951@qq.com>
2018-05-09 15:16:22 +0800

    Initial Commit

---
+++ 第九章 函数/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  第九章 函数
+//
+//  Created by Affee on 2018/5/9.
+//  Copyright © 2018年 affee. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
